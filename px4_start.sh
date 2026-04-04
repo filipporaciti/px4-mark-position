e3340d4d8eeefcd3fc010276a41a2877d845ad94
filipporaciti <filipporaciti15@gmail.com>
2026-04-04 17:29:17 +0200

    add camera link server

--- px4_start.sh
+++ px4_start.sh
@@ -5,6 +5,7 @@ cleanup() {
     pkill -9 -f "gz sim"
     pkill -9 -f "px4"
     pkill -9 -f "ruby"
+    pkill -9 -f "gst-launch-1.0"
     exit
 }
 
@@ -19,6 +20,9 @@ export PX4_GZ_WORLD=aruco_floor
 cd $PX4_PATH
 
 # --- START ---
+echo "Start camera link server on port 5001"
+gst-launch-1.0 -v udpsrc port=5600 ! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! mpegtsmux ! udpsink host=127.0.0.1 port=5001 > /dev/null 2>&1 &
+
 echo "Start PX4 Firmware..."
 make px4_sitl gz_x500_mono_cam_down
 
