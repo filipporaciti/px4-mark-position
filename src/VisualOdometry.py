d5d384b9c8fa0fc799f4fd8c83687384ff7e0976
filipporaciti <filipporaciti15@gmail.com>
2026-04-04 18:04:36 +0200

    draw aruco marker in camera video

--- src/VisualOdometry.py
+++ src/VisualOdometry.py
@@ -1,24 +1,44 @@
 import cv2
+import cv2.aruco as aruco
+
 
 class VisualOdometry:
 
-    def __init__(self, video_url: str):
+    def __init__(self, video_url: str, marker_type: int, show_video: bool = True):
         self.video_url = video_url
+        self.marker_type = marker_type
+        self.show_video = show_video
 
         self.cap = cv2.VideoCapture(self.video_url, cv2.CAP_FFMPEG)
         self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+        self.aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+        self.aruco_params = aruco.DetectorParameters()
+        self.detector = aruco.ArucoDetector(self.aruco_dict, self.aruco_params)
 
         if not self.cap.isOpened():
             raise Exception(f"Error: Unable to connect to stream at {self.video_url}")
 
 
+    def process_frame(self):
+        _, frame = self.cap.read()
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        corners, ids, _ = self.detector.detectMarkers(gray)
+
+        if ids is not None:
+            aruco.drawDetectedMarkers(frame, corners, ids)
+
+        if self.show_video:
+            cv2.imshow('Gazebo Aruco Detection', frame)
+
 
 if __name__ == "__main__":
     video_url = "udp://127.0.0.1:5001?fifo_size=0&overrun_nonfatal=1"
-    vo = VisualOdometry(video_url)
+    marker_type = aruco.DICT_4X4_50
+    vo = VisualOdometry(video_url, marker_type)
 
     while True:
-        ret, frame = vo.cap.read()
-        cv2.imshow('Gazebo Aruco Detection', frame)
+        vo.process_frame()
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
