670c2d553302379ea458971f3cf85e1aabed194e
filipporaciti <filipporaciti15@gmail.com>
2026-04-15 09:48:58 +0200

    refactoring: json marker informations

--- src/VisualOdometry.py
+++ src/VisualOdometry.py
@@ -2,23 +2,25 @@ import cv2
 import cv2.aruco as aruco
 import numpy as np
 import math
+import json
 
 
 class VisualOdometry:
-    marker_length = 0.5 # Marker size in meters
-    obj_points = np.array([
-        [-marker_length/2,  marker_length/2, 0],
-        [ marker_length/2,  marker_length/2, 0],
-        [ marker_length/2, -marker_length/2, 0],
-        [-marker_length/2, -marker_length/2, 0]
-    ], dtype=np.float32)
 
-    def __init__(self, video_url: str, marker_type: int, camera_matrix: np.ndarray, show_video: bool = True, dist_coeff=np.zeros(5)):
+    def __init__(self, video_url: str, marker_type: int, camera_matrix: np.ndarray, show_video: bool = True, dist_coeff=np.zeros(5), marker_info_path="src/marker_info/aruco_sim.json"):
         self.video_url = video_url
         self.marker_type = marker_type
         self.show_video = show_video
         self.camera_matrix = camera_matrix
         self.dist_coeff = dist_coeff
+        self.marker_info = json.load(open(marker_info_path, "r"))
+
+        self.obj_points = np.array([
+            [-self.marker_info["marker_length"]/2,  self.marker_info["marker_length"]/2, 0],
+            [ self.marker_info["marker_length"]/2,  self.marker_info["marker_length"]/2, 0],
+            [ self.marker_info["marker_length"]/2, -self.marker_info["marker_length"]/2, 0],
+            [-self.marker_info["marker_length"]/2, -self.marker_info["marker_length"]/2, 0]
+        ], dtype=np.float32)
 
         self.cap = cv2.VideoCapture(self.video_url, cv2.CAP_FFMPEG)
         self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
@@ -67,7 +69,7 @@ class VisualOdometry:
         for i in range(len(ids)):
 
             img_points = corners[i][0]
-            success, rvec, tvec = cv2.solvePnP(VisualOdometry.obj_points, img_points, self.camera_matrix, self.dist_coeff)
+            success, rvec, tvec = cv2.solvePnP(self.obj_points, img_points, self.camera_matrix, self.dist_coeff)
 
             if success:
                 cv2.drawFrameAxes(frame, self.camera_matrix, self.dist_coeff, rvec, tvec, 0.05)
@@ -75,10 +77,14 @@ class VisualOdometry:
                 R, _ = cv2.Rodrigues(rvec)
                 camera_world_pos = -R.T @ tvec
 
-                camera_world_pos = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]]) @ camera_world_pos
+                if str(ids[i][0]) not in self.marker_info["position"]:
+                    print(f"Warning: Marker ID {ids[i][0]} not found in marker_info. Skipping position adjustment.")
+                    continue
 
-                camera_world_pos[1] = (ids[i][0]%4) * (2) + camera_world_pos[1]
-                camera_world_pos[0] = (ids[i][0]//4) * (2) + camera_world_pos[0]
+                camera_world_pos[0] = self.marker_info["position"][str(ids[i][0])]["x"] + camera_world_pos[0]
+                camera_world_pos[1] = self.marker_info["position"][str(ids[i][0])]["y"] + camera_world_pos[1]
+
+                camera_world_pos = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]]) @ camera_world_pos
 
                 yaw = -math.atan2(R[1, 0], R[0, 0])
 
