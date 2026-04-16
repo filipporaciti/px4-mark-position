709c5e5b9fca10a0ca733ad0d95176c46d4dca4d
filipporaciti <filipporaciti15@gmail.com>
2026-04-16 11:07:08 +0200

    add x500 mono cam up

--- setup/global.sh
+++ setup/global.sh
@@ -7,3 +7,6 @@ git submodule update --init --recursive --force
 cp -r ../my_models/* ./Tools/simulation/gz/models
 
 cp -r ../my_worlds/* ./Tools/simulation/gz/worlds
+
+cp ../my_models/x500_mono_cam_up/4015_gz_x500_mono_cam_up ./ROMFS/px4fmu_common/init.d-posix/airframes
+sed -i '' '84a\'$'\n''4015_gz_x500_mono_cam_up' ./ROMFS/px4fmu_common/init.d-posix/airframes/CMakeLists.txt
