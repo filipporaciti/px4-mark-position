856e1a525396172601a9e8b8424b131b0e33419b
filipporaciti <filipporaciti15@gmail.com>
2026-05-20 10:21:33 +0200

    update PX4-Autopilot submodule

--- setup/macos.sh
+++ setup/macos.sh
@@ -4,15 +4,6 @@ cd PX4-Autopilot
 
 ./Tools/setup/macos.sh --sim-tools
 
-brew install gnu-sed
-
-# BUILD SETUP
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$(brew --prefix qt@5)
-export LIBRARY_PATH=$LIBRARY_PATH:$(brew --prefix)/lib
-
-gsed -i '83c rotor_velocity_message.set_velocity(i, static_cast<double>(outputs[i]));' ./src/modules/simulation/gz_bridge/GZMixingInterfaceESC.cpp
-
-
 echo "Build Firmware"
 make px4_sitl
 
